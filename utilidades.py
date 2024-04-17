93f2246a348a02b4c89f5432c841438adc329c6a
armandossrecife <armando.sousa@gmail.com>
2024-04-17 01:06:17 -0300

    Refatoracao para permitir manipulacao de imagens, audios e videos

--- utilidades.py
+++ utilidades.py
@@ -1,7 +1,17 @@
-ALLOWED_EXTENSIONS = {'jpg', 'png', 'jpeg'}
+ALLOWED_EXTENSIONS = {'jpeg','jpg', 'png', 'mp4', 'mp3'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_file_extension(filename):
     return filename.rsplit('.', 1)[1].lower()            
+
+def get_media_type(extension):
+  media_types = {
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "png": "image/png",
+      "mp4": "video/mp4",
+      "mp3": "audio/mp3"
+  }
+  return media_types.get(extension.lower())
